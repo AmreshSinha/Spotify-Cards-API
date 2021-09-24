db52161bfa52d48e6a3a29593bec072c3055659a
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2021-09-24 20:24:58 +0530

    Updated for Hosting

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const SpotifyWebApi = require('spotify-web-api-node')
 const cors = require('cors');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors({
     origin: '*'
